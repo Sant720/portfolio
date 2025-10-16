527d028732a8e0153a5945de2f47841bde25d541
Sant720 <santiago.bolivar7@gmail.com>
2025-10-16 15:52:28 -0500

    modify styling

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -29,7 +29,7 @@ export function ProjectCard({ project }: Props) {
               alt={name}
               width={500}
               height={300}
-              className="h-40 w-full object-cover object-top"
+              className="h-40 w-full object-cover object-top rounded-lg"
             />
           </Link>
         )}
