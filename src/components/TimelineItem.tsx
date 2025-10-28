0eee5c6eb19d05fc52375b247f79c68ef300cf06
Santiago Bolivar <santiago.bolivar7@gmail.com>
2025-10-28 14:05:35 -0500

    modify TimelineItem mobile layout

--- src/components/TimelineItem.tsx
+++ src/components/TimelineItem.tsx
@@ -13,11 +13,12 @@ export default function TimelineItem({ experience }: Props) {
     experience;
 
   return (
-    <li className="relative grid gap-4 py-4 sm:ml-10 sm:grid-cols-[auto,1fr] sm:grid-rows-[auto auto]">
+    <li className="relative grid grid-cols-[auto,1fr] grid-rows-[auto auto] gap-4 py-4 sm:ml-10 sm:block">
+      {/* Career item logo */}
       <Link
         href={href}
         target="_blank"
-        className="static items-center justify-center rounded-full sm:absolute sm:-left-16 sm:top-4 sm:col-span-1 sm:row-span-1"
+        className="static col-span-1 row-span-1 flex flex-col items-center justify-start rounded-full sm:absolute sm:-left-16 sm:top-4"
       >
         <Avatar className="size-12 border">
           <AvatarImage
@@ -28,30 +29,38 @@ export default function TimelineItem({ experience }: Props) {
           <AvatarFallback>{name[0]}</AvatarFallback>
         </Avatar>
       </Link>
-      <div className="flex flex-1 flex-col justify-start gap-1">
+
+      {/* Career item heading */}
+      <div className="col-span-1 row-span-1 flex min-w-0 flex-col justify-start gap-1 sm:min-w-full">
+        <h2 className="order-1 font-semibold leading-none sm:order-2">
+          {name}
+        </h2>
         {start && (
-          <time className="text-xs text-muted-foreground">
+          <time className="order-2 text-xs text-muted-foreground sm:order-1">
             <span>{start}</span>
             <span>{" - "}</span>
             <span>{end ? end : "Present"}</span>
           </time>
         )}
-        <h2 className="font-semibold leading-none">{name}</h2>
-        {title && <p className="text-sm text-muted-foreground">{title}</p>} 
-      </div>
-      <div className="flex flex-1 flex-col justify-start gap-1 sm:col-span-2 sm:row-start-2">
-        {description && (
-          <ul className="ml-4 list-outside list-disc">
-            {description.map((desc, i) => (
-              <li key={i} className="prose pr-8 text-sm dark:prose-invert">
-                {desc}
-              </li>
-            ))}
-          </ul>
+        {title && (
+          <p className="order-3 text-sm text-muted-foreground sm:order-3">
+            {title}
+          </p>
         )}
       </div>
+
+      {/* Career item description */}
+      {description && (
+        <ul className="col-span-2 ml-4 list-outside list-disc">
+          {description.map((desc, i) => (
+            <li key={i} className="prose pr-4 text-sm dark:prose-invert">
+              {desc}
+            </li>
+          ))}
+        </ul>
+      )}
       {links && links.length > 0 && (
-        <div className="mt-2 flex flex-row flex-wrap items-start gap-2">
+        <div className="col-span-2 mt-2 flex flex-row flex-wrap items-start gap-2 sm:col-span-auto">
           {links?.map((link, idx) => (
             <Link href={link.href} key={idx}>
               <Badge key={idx} title={link.name} className="flex gap-2">
